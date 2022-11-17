0e47bc81a14f111baa852259d65206e29d6f25a4
Bijit Mondal <68532969+Bijit-Mondal@users.noreply.github.com>
2022-11-17 11:10:39 +0000

    Vowel Consnent

--- ShellBasic/VowelConsonent.sh
+++ ShellBasic/VowelConsonent.sh
@@ -1,2 +1,15 @@
-echo "Enter a string "
+echo "Enter a string in lowercase only"
 read str
+len=`echo $str | wc -c`
+v=0
+c=0
+for((i=1; i<len; i++))
+do
+    ch=`echo $str | cut -c $i`
+    case $ch in
+        [aeiou])  v=`expr $v + 1` ;;
+    esac
+done
+c=`expr $len - $v`
+c=`expr $c - 1`
+echo $v $c
